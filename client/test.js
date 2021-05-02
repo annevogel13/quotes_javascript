be6df72a06c46f054d524d8fe8ed9f73b5c66697
p1905532 <anne.merel@etu.univ-lyon1.fr>
2021-05-03 00:34:43 +0200

    derniere changements

--- client/test.js
+++ client/test.js
@@ -1,11 +1,11 @@
 suite('Tests pour la function vote', function () {
     test("On vérifie si le resultat d'une duel est bien pris en compte", function () {
         fetchTousCitations().then(data => {
-            citation1 = data[0];
-            citation2 = data[1];
+            citation1 = data[12];
+            citation2 = data[13];
 
             prendsEnCompteVote(citation1._id, citation2._id);
-
+            
             fetchUneCitationById(citation1._id).then(citation => {
                 
                 // test marche seulement quand il y a pas des autres duels fait --> donc au debut de chaque 30 min 
